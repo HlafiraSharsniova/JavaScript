9400c092d7ef8571913514cff6ba60a856e968e3
Hlafira Sharsniova <Hlafira.Sharsniova@microsoft.wsei.edu.pl>
2024-01-24 19:14:29 +0100

    done

--- Lab4/main.js
+++ Lab4/main.js
@@ -1,62 +1,185 @@
-const notesEl = document.querySelector('.notes');
-const addBtn = document.querySelector('.note-add')
+let notes = [];
+let tags = [];
+const addNote = document.querySelector("#submit");
+const btnShowForm = document.querySelector(".add-note");
+const form = document.querySelector(".form");
+const overlay = document.querySelector(".overlay");
+const editOverlay = document.querySelector(".edit-overlay");
+const addNoteBtn = document.querySelector(".add-note");
+const addTagBtn = document.querySelector(".add-tag");
+const notesList = document.querySelector(".notes-list");
+const editForm = document.querySelector(".edit-form");
+const noteSearch = document.querySelector(".note-search");
+const noteSearchTag = noteSearch.querySelector('input[type="text"]');
+const tagsMenu = document.querySelector("#tags");
+const notesMenu = document.querySelector("#notes");
 
-function createNote(title, text) {
-  const noteEl = document.createElement('div');
-  noteEl.classList.add('note');
-  noteEl.innerHTML = `
-    <div class="note-header">
-      <p id="note-title">${title}</p>
-      <textarea id="note-title-input" class="hidden">${title}</textarea>
-      <div>
-        <button class="note-edit"><i class="fa-solid fa-pen-to-square"></i></button>
-        <button class="note-delete"><i class="fa-solid fa-trash"></i></button>
-        <button class="note-color" style="display: block;" onclick="changeColor();"><i class="fa-solid fa-palette"></i></button>
-      </div>
-    </div>
-    <p id="note-text">${text}</p>
-    <textarea id="note-textarea" class="hidden">${text}</textarea>
+const loadNotes = () => {
+  const jsonNotes = JSON.parse(window.localStorage.getItem("notes"));
+  jsonNotes ? (notes = [...jsonNotes]) : console.log("Brak notatek");
+};
+loadNotes();
 
-  `
-
-  const editBtn = noteEl.querySelector('.note-edit');
-  const deleteBtn = noteEl.querySelector('.note-delete');
-  const titleEl = noteEl.querySelector('#note-title');
-  const textEl = noteEl.querySelector('#note-text');
-  const titleInputEl = noteEl.querySelector('#note-title-input');
-  const textInputEl = noteEl.querySelector('#note-textarea');
-
-  editBtn.addEventListener('click', (e) => {
-    titleEl.classList.toggle('hidden');
-    textEl.classList.toggle('hidden');
-
-    titleInputEl.classList.toggle('hidden');
-    textInputEl.classList.toggle('hidden');
-  });
-
-  deleteBtn.addEventListener('click', (e) => {
-    noteEl.remove();
-  });
-  
-  // let colorBtn = noteEl.getElementById('.note-color');
-  // let colorValue = colorBtn.value;
-
-  // colorBtn.addEventListener("input", () => {
-  //   noteEl.getElementById("edit-note").style.background = colorBtn.value;
-  // });
-
-  titleInputEl.addEventListener('input', (e) => {
-    titleEl.innerText = e.target.value;
-  });
-
-  textInputEl.addEventListener('input', (e) => {
-    textEl.innerText = e.target.value;
-  });
-
-  return noteEl;
+function returnDate(date) {
+  const day = `${date.getDate()}`.padStart(2, "0");
+  const month = `${date.getMonth() + 1}`.padStart(2, "0");
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
 }
 
-addBtn.addEventListener('click', (e) => {
-  const el = createNote("Title", "Content");
-  notesEl.appendChild(el);
+const toggleEditForm = () => {
+  editForm.classList.toggle("hidden");
+  editOverlay.classList.toggle("hidden");
+};
+
+const displayNotes = (notes) => {
+  notesList.innerHTML = "";
+
+  if (notes.some((acc) => acc.pin)) {
+    notes.sort((a, b) => b.pin - a.pin);
+  }
+
+  notes.forEach((note) => {
+    const newNote = document.createElement("div");
+    newNote.classList.add("note");
+    newNote.style.backgroundColor = note.color;
+    newNote.innerHTML = `<div class='manage-icons'> 
+      <div>
+      <button class="edit-button"><i class="fa-solid fa-pen-to-square"></i></button> 
+      <button class="delete-button"><i class="fa-solid fa-trash"></i></button>
+      </div>
+    </div>
+    <div class='pin-icon'>${note.pin ? "🔴" : "⚪"}</div>
+    <div>
+      <h1>${note.title}</h1> 
+      <div class="note--content">
+        <div class="note--tags">Tagi: ${note.tags} </div>
+        ${note.content}
+    </div>
+    </div>
+    <div class="note--date">Data utworzenia: ${returnDate(
+      new Date(note.dateOfCreation)
+    )}
+   <div>`;
+
+    const notePin = newNote.querySelector(".pin-icon");
+    newNote
+      .querySelector(".delete-button")
+      .addEventListener("click", () => deleteNote(note.id));
+    newNote
+      .querySelector(".edit-button")
+      .addEventListener("click", () => toggleEditMenu(note.id));
+    notePin.addEventListener("click", () => pinNote(note, notePin));
+    notesList.appendChild(newNote);
+  });
+};
+displayNotes(notes);
+
+const pinNote = function (note, notePin) {
+  if (notePin.textContent === "⚪") {
+    const findNote = notes.find((n) => n.id === Number(note.id));
+    findNote.pin = true;
+    localStorage.setItem("notes", JSON.stringify(notes));
+    displayNotes(notes);
+  }
+
+  if (notePin.textContent === "🔴") {
+    const findNote = notes.find((n) => n.id === Number(note.id));
+    findNote.pin = false;
+    localStorage.setItem("notes", JSON.stringify(notes));
+    displayNotes(notes);
+  }
+};
+
+const deleteNote = function (id) {
+  notes = notes.filter((note) => note.id !== Number(id));
+  localStorage.setItem("notes", JSON.stringify(notes));
+  displayNotes(notes);
+};
+
+const loadValuesToForm = (note) => {
+  editForm.querySelector('input[name="title"]').value = note.title;
+  editForm.querySelector("textarea").value = note.content;
+  editForm.querySelector('input[name="tags"]').value = String(note.tags).replaceAll(",", " ");
+  editForm.querySelector(`input[type="color"]`).value = note.color;
+};
+
+const toggleEditMenu = function (id) {
+  toggleEditForm();
+  const singleNote = notes.find((note) => note.id === Number(id));
+  console.log(singleNote);
+  loadValuesToForm(singleNote);
+
+  document.querySelector(".edit-note").addEventListener("click", function () {
+    editNote(singleNote);
+  });
+  displayNotes(notes);
+};
+
+const editNote = function (newNote) {
+  newNote.title = editForm.querySelector('input[name="title"]').value;
+  newNote.content = editForm.querySelector("textarea").value;
+  newNote.tags = editForm.querySelector('input[name="tags"]').value;
+  newNote.color = editForm.querySelector('input[type="color"]').value;
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
+
+addNote.addEventListener("click", (e) => {
+  e.preventDefault();
+  const title = document.querySelector('input[name="title"]').value;
+  const content = document.querySelector("textarea").value;
+  const color = document.querySelector('input[type="color"]').value;
+  const tags = document.querySelector('input[name="tags"]').value.split(" ");
+  const note = new Note(
+    Date.now(),
+    title,
+    content,
+    color,
+    false,
+    tags,
+    Date.now()
+  );
+  notes.push(note);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  toggleForm();
+  displayNotes(notes);
 });
+
+noteSearchTag.addEventListener("input", function (e) {
+  let filteredNotes;
+  setTimeout(() => {
+    if (e.target.value === "") displayNotes(notes);
+    else {
+      filteredNotes = notes.filter((note) =>
+        note.tags.includes(e.target.value)
+      );
+      displayNotes(filteredNotes);
+    }
+  }, 500);
+});
+
+const toggleForm = () => {
+  form.classList.toggle("hidden");
+  overlay.classList.toggle("hidden");
+};
+
+const showNotes = () => {
+  addNoteBtn.classList.remove("hidden");
+  notesList.classList.remove("hidden");
+};
+
+btnShowForm.addEventListener("click", toggleForm);
+overlay.addEventListener("click", toggleForm);
+editOverlay.addEventListener("click", toggleEditForm);
+
+class Note {
+  constructor(id, title, content, color, pin, tags, dateOfCreation) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+    this.color = color;
+    this.pin = pin;
+    this.tags = tags;
+    this.dateOfCreation = dateOfCreation;
+  }
+}
